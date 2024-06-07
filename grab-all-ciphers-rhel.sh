77ea7259bd115260a7a221e47d5d129d304dd9f8
root <root@TRX-SL-IT-LP1>
2024-06-07 13:31:21 +0530

    modified:   grab-all-ciphers-rhel.sh

--- grab-all-ciphers-rhel.sh
+++ grab-all-ciphers-rhel.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Function to get the server name
+get_server_name() {
+    hostname
+}
+
 # Check if OpenSSL is installed
 if ! command -v openssl &> /dev/null
 then
@@ -7,9 +12,14 @@ then
     exit 1
 fi
 
+# Grab the server name
+server_name=$(get_server_name)
+
 # Grab all the available ciphers
 ciphers=$(openssl ciphers -v)
 
-# Print the ciphers list to the terminal
+# Print the server name and the ciphers list to the terminal
+echo "Server Name: $server_name"
 echo "Available ciphers on this RHEL system:"
 echo "$ciphers"
+
